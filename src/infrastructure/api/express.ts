08dbecb1736a69bfda2c28e2b4262552ae35f46d
Marcelo Felix Salgado <marcelo.felixs@gmail.com>
2022-10-18 23:00:50 -0300

    Creating the initial structure of the project

---
+++ src/infrastructure/api/express.ts
@@ -0,0 +1,20 @@
+import express, { Express } from "express";
+import { Sequelize } from "sequelize-typescript";
+
+export const app: Express = express();
+app.use(express.json());
+// app.use("/customer", customerRoute);
+// app.use("/product", productRoute);
+
+export let sequelize: Sequelize;
+
+async function setupDb() {
+  sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: ":memory:",
+    logging: false,
+  });
+  // await sequelize.addModels([CustomerModel, ProductModel]);
+  await sequelize.sync();
+}
+setupDb();
